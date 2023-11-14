963c11c99da0631f712c123bb77ae4061fb78c5c
a-ryang <chnwu.kim@gmail.com>
2023-11-15 08:43:22 +0900

    chore: SpaWebFilter #3

---
+++ todo/src/main/java/com/aryang/todo/global/web/SpaWebFilter.java
@@ -0,0 +1,28 @@
+package com.aryang.todo.global.web;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SpaWebFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+
+        if(!path.startsWith("/api") &&   // 경로가 "/api"로 시작하지 않는 경우
+            !path.contains(".") &&       // 경로에 점(.)이 포함되지 않은 경우를 확인. 정적 파일(예: .css, .js)을 필터링에서 제외하기 위함
+            path.matches("/(.*)"))       // 경로가 "/"로 시작하는지 확인
+        {
+            request.getRequestDispatcher("/").forward(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
