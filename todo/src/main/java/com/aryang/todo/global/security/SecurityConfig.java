e9cfc904563fb2f051ea095b3c8f38672fbfee66
a-ryang <chnwu.kim@gmail.com>
2023-11-15 21:11:50 +0900

    chore: SpaWebFilter #3

--- todo/src/main/java/com/aryang/todo/global/security/SecurityConfig.java
+++ todo/src/main/java/com/aryang/todo/global/security/SecurityConfig.java
@@ -1,28 +1,39 @@
 package com.aryang.todo.global.security;
 
+import static org.springframework.http.HttpMethod.*;
+
+import com.aryang.todo.global.web.SpaWebFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final SpaWebFilter spaWebFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers(HttpMethod.POST, "/api/sign-up" ).permitAll()
+                .requestMatchers( GET, "/", "/index.html", "/static/**", "/assets/**", "*.ico").permitAll()
+                .requestMatchers(OPTIONS, "/**").permitAll()
+                .requestMatchers("/api/todos").permitAll()
+                .requestMatchers(POST, "/api/sign-up" ).permitAll()
                 .anyRequest().authenticated()
             )
-            .csrf(AbstractHttpConfigurer::disable);
+            .csrf(AbstractHttpConfigurer::disable)
+            .addFilterAfter(spaWebFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
